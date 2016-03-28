df59cfc622b9a6bfdbe836455a0d72444a1d44b6
Yang Ming <yangming0116@163.com>
2016-03-28 23:25:30 +0800

    添加配置文件样例

---
+++ sample.config.php
@@ -0,0 +1,19 @@
+<?php
+//是否开启调试模式
+$cfg['main']['debug'] = 0;
+//session域
+$cfg['main']['domain'] = '.webnote.snje.org';
+//文件系统的编码
+$cfg['main']['fsencoding'] = 'utf-8';
+
+/*****以下不要修改*****/
+$cfg['main']['encoding'] = 'utf-8';
+$cfg['main']['db'] = '';
+$cfg['main']['theme'] = 'def';
+$cfg['route'] = ['call'];
+$cfg['mysql'] = [];
+$cfg['sqlite'] = [];
+$cfg['save'] = [
+    'data' => '/data',
+];
+$cfg['upload'] = [];
