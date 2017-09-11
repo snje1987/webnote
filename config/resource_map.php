7574a6d18fb2ced309d753b45c10c67f0eb26a8a
Yang Ming <yangming0116@163.com>
2017-09-11 21:01:05 +0800

    升级框架

--- config/resource_map.php
+++ config/resource_map.php
@@ -1,57 +1,40 @@
 <?php
+
 $value = [];
 $value[] = [
     'method' => 'copy',
     'type' => 'file',
-    'from' => [
-        '/vendor/components/jquery/jquery.js',
-        '/vendor/components/jquery/jquery.min.js',
-        '/vendor/components/jquery/jquery.min.map',
-        '/vendor/jquery-form/form/dist/jquery.form.min.js',
-        '/vendor/components/highlightjs/highlight.pack.min.js',
-        '/vendor/components/highlightjs/styles/github.css',
-    ],
-    'to' => [
-        '/www/lib/jquery.js',
-        '/www/lib/jquery.min.js',
-        '/www/lib/jquery.min.map',
-        '/www/lib/jquery.form.min.js',
-        '/www/lib/highlight.pack.min.js',
-        '/www/lib/github.css',
+    'map' => [
+        '/www/lib/jquery.js' => '/vendor/components/jquery/jquery.js',
+        '/www/lib/jquery.min.js' => '/vendor/components/jquery/jquery.min.js',
+        '/www/lib/jquery.min.map' => '/vendor/components/jquery/jquery.min.map',
+        '/www/lib/jquery.form.min.js' => '/vendor/jquery-form/form/dist/jquery.form.min.js',
+        '/www/lib/highlight.pack.min.js' => '/vendor/components/highlightjs/highlight.pack.min.js',
+        '/www/lib/github.css' => '/vendor/components/highlightjs/styles/github.css',
     ],
 ];
 $value[] = [
     'method' => 'copy',
     'type' => 'dir',
-    'from' => [
-        '/vendor/twbs/bootstrap/dist',
-        '/vendor/bootstrap-select/bootstrap-select/dist',
-        '/vendor/fortawesome/font-awesome/css',
-        '/vendor/fortawesome/font-awesome/fonts',
-    ],
-    'to' => [
-        '/www/lib/bootstrap',
-        '/www/lib/bootstrap-select',
-        '/www/lib/font-awesome/css',
-        '/www/lib/font-awesome/fonts',
+    'map' => [
+        '/www/lib/bootstrap' => '/vendor/twbs/bootstrap/dist',
+        '/www/lib/bootstrap-select' => '/vendor/bootstrap-select/bootstrap-select/dist',
+        '/www/lib/font-awesome/css' => '/vendor/fortawesome/font-awesome/css',
+        '/www/lib/font-awesome/fonts' => '/vendor/fortawesome/font-awesome/fonts',
     ],
 ];
 $value[] = [
     'method' => 'cssmin',
     'type' => 'dir',
-    'from' => '/theme/def/style',
-    'to' => '/www/theme/def/style',
+    'map' => [
+        '/www/theme/def/style' => '/theme/def/style',
+    ],
 ];
 $value[] = [
     'method' => 'uglify',
     'type' => 'dir',
-    'from' => '/theme/def/script',
-    'to' => '/www/theme/def/script',
-];
-$value[] = [
-    'method' => 'cssmin',
-    'type' => 'dir',
-    'from' => '/theme/def/style',
-    'to' => '/www/theme/def/style',
+    'map' => [
+        '/www/theme/def/script' => '/theme/def/script',
+    ],
 ];
 return $value;
